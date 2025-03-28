7486df10653e82050f77099651a4e3c61b3cd954
lavliekstocode <lavanya.vinodkumar19@gmail.com>
2025-03-28 22:48:10 +0530

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
-
-from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+from .models import CustomUser, Document
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     fieldsets = UserAdmin.fieldsets + (
         ('Custom Fields', {'fields': ('role', 'school', 'resume')}),
     )
+    list_display = ('username', 'email', 'role', 'school')
+    list_filter = ('role',)
+    search_fields = ('username', 'email')
+
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'uploaded_at')
+    search_fields = ('title',)
 
 admin.site.register(CustomUser, CustomUserAdmin)
-
+admin.site.register(Document, DocumentAdmin)
