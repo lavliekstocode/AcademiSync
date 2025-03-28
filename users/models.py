435125ab25c84045830ba70fda1eb0b3dd8f4a41
lavliekstocode <lavanya.vinodkumar19@gmail.com>
2025-03-28 22:48:41 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class CustomUser(AbstractUser):
     ADMIN = 'admin'
@@ -13,20 +14,16 @@ class CustomUser(AbstractUser):
     ]
     
     role = models.CharField(max_length=20, choices=USER_ROLES, default=STUDENT)
-    school = models.CharField(max_length=255, blank=True, null=True)  # Add this field
-    resume = models.FileField(upload_to='resumes/', blank=True, null=True)  # Add this field
+    school = models.CharField(max_length=255, blank=True, null=True)
+    resume = CloudinaryField("file", blank=True, null=True)
 
     def __str__(self):
         return f"{self.username} ({self.role})"
 
-from django.db import models
-from cloudinary.models import CloudinaryField
-
 class Document(models.Model):
     title = models.CharField(max_length=255)
-    file = CloudinaryField("document")  # Automatically uploads to Cloudinary
+    file = CloudinaryField("document")
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
-
